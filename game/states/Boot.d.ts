ce1609a3eab973f9b120940e292d1f17a8973499
Dani <Daniel@imac.local>
2015-04-29 11:27:05 -0300

    Removed .js .map and .d.ts from repo

--- game/states/Boot.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../lib/phaser.d.ts" />
-/// <reference path="Preloader.d.ts" />
-declare module Superhero {
-    class Boot extends Phaser.State {
-        preload(): void;
-        create(): void;
-    }
-}
