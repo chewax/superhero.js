862a62195568e249ec76f2c7f1bee014a6fc5ace
Dani <Daniel@imac.local>
2015-05-03 00:50:45 -0300

    removed preloading of unused assets

--- game/states/Preloader.ts
+++ game/states/Preloader.ts
@@ -24,23 +24,27 @@ module Superhero {
 
         loadAssets () {
 
+            //Spritesheets
             this.game.load.atlasJSONHash('hero1', '/assets/hero1.png', '/assets/hero1.json');
             this.game.load.atlasJSONHash('badie', '/assets/hero3.png', '/assets/hero3.json');
             this.game.load.atlasJSONHash('heart', '/assets/heart.png', '/assets/heart.json');
             this.game.load.atlasJSONHash('meteors', '/assets/meteors.png', '/assets/meteors.json');
             this.game.load.atlasJSONHash('bullets', '/assets/bullets.png', '/assets/bullets.json');
             this.game.load.atlasJSONHash('env', '/assets/environment.png', '/assets/environment.json');
-            this.game.load.image('background', '/assets/Background.png');
+
+            //Single Images
             this.game.load.image('fuelbar', '/assets/fuel.png');
             this.game.load.image('farback', '/assets/farback.gif');
             this.game.load.image('starfield', '/assets/starfield.png');
-            this.game.load.image('shadow', '/assets/shadow.png');
             this.game.load.image('obstacleItem', '/assets/placeholder.png');
+
+
+            //Fonts
             this.game.load.bitmapFont('desyrel', 'assets/fonts/bitmapFonts/desyrel.png', 'assets/fonts/bitmapFonts/desyrel.xml');
 
+            //Audio
             //this.game.load.audio('theme', '/assets/sounds/demon_destroyer.mp3');
             this.game.load.audio('theme', '/assets/sounds/demon_destroyer.ogg');
-
 
             Gamepads.GamePad.preloadAssets(this.game, '/assets');
 
